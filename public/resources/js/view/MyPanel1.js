fbdc749e69b69c179d4292f16cf5aa44cd2a1049
www.jamesjara.com <jamesjara@gmail.com>
2012-11-24 13:09:19 -0600

    Initial commit

--- public/resources/js/view/MyPanel1.js
+++ public/resources/js/view/MyPanel1.js
@@ -19,16 +19,42 @@ Ext.define('JWF.view.MyPanel1', {
     config: {
         fullscreen: true,
         items: [
-            {
-                xtype: 'nestedlist',
-                styleHtmlContent: true,
-                scrollable: true,
-                detailCard: {
-                    xclass: 'JWF.view.Form'
-                },
-                store: 'MyJsonTreeStore',
-                title: 'TOPs'
-            }
+{
+                    xtype: 'nestedlist',
+                    title: 'TOPs',
+                    iconCls: 'star',
+                    displayField: 'title',
+                    store: {
+                        type: 'tree',
+                        fields: [
+                            'title', 'link', 'author', 'contentSnippet', 'content',
+                            {name: 'leaf', defaultValue: true}
+                        ],
+                        root: {
+                            leaf: false
+                        },
+                        proxy: {
+							type: 'jsonp',
+							url: 'http://news.innosystem.org/data.php',
+							reader: {
+								type: 'json',
+								rootProperty: 'data'
+							}
+                        }
+                    },
+
+                    detailCard: {
+                        xtype: 'panel',
+                        scrollable: true,
+                        styleHtmlContent: true
+                    },
+
+                    listeners: {
+                        itemtap: function(nestedList, list, index, element, post) {
+                            this.getDetailCard().setHtml(post.get('content'));
+                        }
+                    }
+                }
         ]
     }
 
